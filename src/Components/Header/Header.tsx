e5b83667a071293c17dc926f6a0c296da562d8a6
Leon Huston <lhuston848@gmail.com>
2019-06-24 23:21:59 +1200

    Header component init

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <div className="header">
+      <h1 className="headerText">How old?</h1>
+    </div>
+  );
+};
